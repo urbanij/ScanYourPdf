b31facc60d0afe1adece433c612f41fa22efb219
Francesco Urbani <francescourbanidue@gmail.com>
2020-05-28 09:34:48 +0200

    initial commit.

---
+++ database.py
@@ -0,0 +1,41 @@
+import psycopg2
+
+class Database:
+    def __init__(self, name):
+        self._conn = psycopg2.connect(name)
+        self._cursor = self._conn.cursor()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.commit()
+        self.connection.close()
+
+    @property
+    def connection(self):
+        return self._conn
+
+    @property
+    def cursor(self):
+        return self._cursor
+
+    def commit(self):
+        self.connection.commit()
+
+    def execute(self, sql, params=None):
+        self.cursor.execute(sql, params or ())
+
+    def fetchall(self):
+        return self.cursor.fetchall()
+
+    def fetchone(self):
+        return self.cursor.fetchone()
+
+    def query(self, sql, params=None):
+        self.cursor.execute(sql, params or ())
+        return self.fetchone()
+
+    def query_all(self, sql, params=None):
+        self.cursor.execute(sql, params or ())
+        return self.fetchall()
---
+++ readme.txt
@@ -0,0 +1,4 @@
+Telegram bot for turning PDFs into scanned-looking PDFs.
+
+(Inspired by https://gitlab.com/edouardklein/falsisign)
+
